63909ae31d84c8f3ace5d11cbe6f3ac8ab8778fd
Filip14720 <filip14720@hotmail.com>
2024-02-13 16:28:16 +0100

    Dodate metode za update korisnickih podataka

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Laravel\Sanctum\PersonalAccessToken;
 
 class UserController extends Controller
 {
@@ -17,5 +18,44 @@ class UserController extends Controller
         $user = User::find($id);
 
         return response()->json(['user' => $user], 200); 
+    }
+
+    public function GetUserDataByToken(Request $request)
+    {
+        $user = $this->getTokenFromRequest($request);
+
+        return response()->json(['user' => $user], 200); 
+    }
+
+    public function getTokenFromRequest(Request $request)
+    {
+        $authorizationHeader = $request->header('Authorization');
+
+        if ($authorizationHeader && str_starts_with($authorizationHeader, 'Bearer ')) 
+        {            
+            $tokenString = substr($authorizationHeader, 7);
+            $token = PersonalAccessToken::findToken($tokenString);
+
+            if ($token) 
+            {
+                $user = $token->tokenable;
+                return $user;
+            } 
+            else 
+            {
+                return null;
+            }
+        }
+
+        return null; 
+    }
+
+    public function update(Request $request)
+    {
+        $user = $this->getTokenFromRequest($request);
+
+        $user->update($request->all());
+
+        return response($user, 200);
     }
 }
